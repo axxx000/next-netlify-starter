0dc3fecc5350b8ab738c0a39f5c2f5edc4ca3df8
Adrian Smith <adrian.kehm.smith@gmail.com>
2023-06-10 09:37:27 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,25 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Quiet Projects</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to Quiet Projects." />
         <p className="description">
-        <code>pages/index.js</code>
+        <code><p>A Freelance Business dedicated to helping individuals get their thing online.</p>
+
+        Below is an example of the recent work completed 🚀
+        <p>
+        <li>Custom built in <a href="http://cargocollective.com">Cargo Collective</a> using Javascript and Jquery.  
+          <ul>• <a href="http://thomasjpg.com">Thomasjpg</a></ul>
+          <ul>• <a href="http://henrykingcreative.com">Henry King</a></ul>
+          <ul>• <a href="http://gracemoorefilm.com">Grace Moore</a></ul>
+
+        </li>
+        </p>
+        </code>
         </p>
       </main>
 
